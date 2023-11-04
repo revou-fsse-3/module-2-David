a0f3e7f82b78fc3c24fa23ce8dfc65db52dba879
David - Revo U - Seoul - Team 2 <davidwahyuds@gmail.com>
2023-11-04 09:57:35 +0700

    deploying

---
+++ script.js
@@ -0,0 +1,12 @@
+function openNav() {
+  document.getElementById("mobileSidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mobileSidenav").style.width = "0";
+}
+
+function showAlert() {
+    alert(
+      "Thank you for filling the form!"
+    );

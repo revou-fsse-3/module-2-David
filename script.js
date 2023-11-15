377cc0b886fb0eb519d9bec5c0413431ca842f7b
David - Revo U - Seoul - Team 2 <davidwahyuds@gmail.com>
2023-11-15 20:48:12 +0700

    fixing background colour

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const navbar = document.getElementById("navbar");
 const headerSec1 = document.getElementById("headerSec");
 const bodybg = document.getElementById("bodyid");
 const formdm = document.getElementById("formconId");
+const formBtn = document.getElementById("buttonSbmt");
+const sec1a = document.getElementById("bgliSec1a");
+const sec1b = document.getElementById("bgliSec1b");
+
 let darkMode = localStorage.getItem("dark-mode");
 
 const enableDarkMode = () => {
@@ -11,6 +15,11 @@ const enableDarkMode = () => {
   headerSec1.classList.add("headersecDM");
   bodybg.classList.add("bodyDM");
   formdm.classList.add("formconDM");
+  formBtn.classList.add("buttonForm");
+  sec1a.classList.remove("bglisc1");
+  sec1a.classList.add("sec1DMa");
+  sec1b.classList.remove("bglisc1");
+  sec1b.classList.add("sec1DMb");
   //toggleBtn.classList.remove("dark-mode-toggle");
   localStorage.setItem("dark-mode", "enabled");
 };
@@ -20,6 +29,11 @@ const disableDarkMode = () => {
   headerSec1.classList.remove("headersecDM");
   bodybg.classList.remove("bodyDM");
   formdm.classList.remove("formconDM");
+  sec1a.classList.remove("sec1DMa");
+  sec1a.classList.add("bglisc1");
+  sec1b.classList.remove("sec1DMb");
+  sec1b.classList.add("bglisc1");
+  formBtn.classList.remove("buttonForm");
   //toggleBtn.classList.add("dark-mode-toggle");
   localStorage.setItem("dark-mode", "disabled");
 };
